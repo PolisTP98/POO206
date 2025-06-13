7c3cb744dc5dd4d8ba3de16328e2007cb31830b7
PolisTP98 <123047038@upq.edu.mx>
2025-06-12 20:22:34 -0600

    Práctica_7_POO

--- FlaskProyect/app.py
+++ FlaskProyect/app.py
@@ -2,9 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Ruta simple
 @app.route("/")
 def home():
-    return  "¡Hola mundo Flask!"
+    return "¡Hola mundo Flask!"
+
+# Ruta con parámetros
+@app.route("/saludar/<nombre>")
+def saludar(nombre):
+    return f"¡Hola {nombre}!"
+
+# Ruta try - catch
+@app.errorhandler(404)
+def paginaNoEncontrada(e):
+    return "¡Cuidado, error de capa 8!", 404
+
+# Ruta doble
+@app.route("/usuario")
+@app.route("/usuaria")
+def dobleRoute():
+    return "Soy el mismo recurso del servidor"
+
+# Ruta POST
+@app.route("/formulario", methods = ["POST"])
+def formulario():
+    return "Soy un formulario"
 
 if __name__ == "__main__":
     app.run(port = 3000, debug = True)
